515422235f99d16ccc55b36ec39375434555d3b5
Claire Price <claire.price@mail.com>
2024-08-25 22:24:41 +0100

    Add validation to passwords on frontend sign up form

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -11,37 +11,33 @@ const isInvalidEmail = (email: string) => {
     }
 }
 
-
-
-// function validateEmail(inputText) {
-//     var mailFormat =  /\S+@\S+\.\S+/;
-//     if (inputText.value.match(mailFormat)) {
-//       alert("Valid address!");
-//       return true;
-//     } else {
-//       alert("Invalid address!");
-//       return false;
-//     }
-//   }
-
-
-
-
+const isInvalidPass2 = (pass1: string, pass2: string) => {
+    if (pass1 !== pass2) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 const SignUp = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [secondPassword, setSecondPassword] = useState("");
+
     const [submitClickedName, setSubmitClickedName] = useState(false);
     const [submitClickedEmail, setSubmitClickedEmail] = useState(false);
     const [submitClickedUsername, setSubmitClickedUsername] = useState(false);
     const [submitClickedPassword, setSubmitClickedPassword] = useState(false);
+    const [submitClickedSecondPassword, setSubmitClickedSecondPassword] = useState(false);
 
     const isErrorName = name === "" && submitClickedName;
     const isErrorEmail = isInvalidEmail(email) && submitClickedEmail;
     const isErrorUsername = username === "" && submitClickedUsername;
     const isErrorPassword = password === "" && submitClickedPassword;
+    const isErrorSecondPassword =
+        isInvalidPass2(password, secondPassword) && submitClickedSecondPassword;
 
     const onChangeName = (e: any) => {
         setSubmitClickedName(false);
@@ -63,19 +59,32 @@ const SignUp = () => {
         setPassword(e.target.value);
     };
 
+    const onChangeSecondPassword = (e: any) => {
+        setSubmitClickedSecondPassword(false);
+        setSecondPassword(e.target.value);
+    };
+
     const onSubmit = () => {
         setSubmitClickedName(true);
         setSubmitClickedEmail(true);
         setSubmitClickedUsername(true);
         setSubmitClickedPassword(true);
+        setSubmitClickedSecondPassword(true);
 
-        if (name === "" || isInvalidEmail(email) || username === "" || password === "") {
-            console.log("ERROR");
+        if (
+            name === "" || 
+            isInvalidEmail(email) || 
+            username === "" || 
+            password === "" ||
+            secondPassword !== password ||
+            secondPassword === "" 
+        ) {
+            return;
         } else {
             axios.post("http://localhost:3025/auth/sign-up", {
-                name, 
-                email, 
-                username, 
+                name,
+                email,
+                username,
                 password,
             }).then((res) => {
                 console.log("RESPONSE", res);
@@ -83,49 +92,59 @@ const SignUp = () => {
                 setEmail("");
                 setUsername("");
                 setPassword("");
+                setSecondPassword("");
                 setSubmitClickedName(false);
                 setSubmitClickedEmail(false);
                 setSubmitClickedUsername(false);
                 setSubmitClickedPassword(false);
+                setSubmitClickedSecondPassword(false);
             });
         }
     };
-        
+
     return (
         <Box>
             <Heading textAlign="center" mb={4}>Create an Account</Heading>
             <Box maxW="75%" display="flex" flexDirection="column" alignItems="center" margin="0 auto" gap="4">
-                
+
                 <FormControl isInvalid={isErrorName} isRequired>
-                <FormLabel>Name</FormLabel>
-                <Input type='text' value={name} onChange={onChangeName} />
-                {!isErrorName ? null : (
-                    <FormErrorMessage>Name is required.</FormErrorMessage>
-                )}
+                    <FormLabel>Name</FormLabel>
+                    <Input type='text' value={name} onChange={onChangeName} />
+                    {!isErrorName ? null : (
+                        <FormErrorMessage>Name is required</FormErrorMessage>
+                    )}
                 </FormControl>
 
                 <FormControl isInvalid={isErrorEmail} isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input type='email' value={email} onChange={onChangeEmail} />
-                {!isErrorEmail ? null : (
-                    <FormErrorMessage>A valid email is required.</FormErrorMessage>
-                )}
+                    <FormLabel>Email</FormLabel>
+                    <Input type='email' value={email} onChange={onChangeEmail} />
+                    {!isErrorEmail ? null : (
+                        <FormErrorMessage>A valid email is required</FormErrorMessage>
+                    )}
                 </FormControl>
 
                 <FormControl isInvalid={isErrorUsername} isRequired>
-                <FormLabel>Username</FormLabel>
-                <Input type='text' value={username} onChange={onChangeUsername} />
-                {!isErrorUsername ? null : (
-                    <FormErrorMessage>Username is required.</FormErrorMessage>
-                )}
+                    <FormLabel>Username</FormLabel>
+                    <Input type='text' value={username} onChange={onChangeUsername} />
+                    {!isErrorUsername ? null : (
+                        <FormErrorMessage>Username is required</FormErrorMessage>
+                    )}
                 </FormControl>
 
                 <FormControl isInvalid={isErrorPassword} isRequired>
-                <FormLabel>Password</FormLabel>
-                <Input type='password' value={password} onChange={onChangePassword} />
-                {!isErrorPassword ? null : (
-                    <FormErrorMessage>Password is required.</FormErrorMessage>
-                )}
+                    <FormLabel>Password</FormLabel>
+                    <Input type='password' value={password} onChange={onChangePassword} />
+                    {!isErrorPassword ? null : (
+                        <FormErrorMessage>Password is required</FormErrorMessage>
+                    )}
+                </FormControl>
+
+                <FormControl isInvalid={isErrorSecondPassword} isRequired>
+                    <FormLabel>Enter Password Again</FormLabel>
+                    <Input type='password' value={secondPassword} onChange={onChangeSecondPassword} />
+                    {!isErrorSecondPassword ? null : (
+                        <FormErrorMessage>Passwords must match</FormErrorMessage>
+                    )}
                 </FormControl>
 
                 <Button w="100%" onClick={onSubmit}>Submit</Button>
