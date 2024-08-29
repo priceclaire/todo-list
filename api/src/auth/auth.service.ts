41a3093f8c3d9c941e6a0b54bc3bfedba976965f
Claire Price <claire.price@mail.com>
2024-08-30 00:51:26 +0100

    create hashed passwords using bcrypt

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService) {}
 
-    signUp(signUpDto) {
-        console.log('SIGN UP DATA', signUpDto);
+    async hashPassword(password: string) {
+        const saltRounds = 10;
+        return await bcrypt.hash(password, saltRounds);
+    }
+
+    async signUp(signUpDto) {
+        const hashedPassword = await this.hashPassword(signUpDto.password);
+        console.log('HASHED PASSWORD', hashedPassword);
+        signUpDto.password = hashedPassword;
+        this.userService.createUser(signUpDto);
         return "fake token 2";
     }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignUpDto } from 'src/auth/auth.controller';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    async createUser(user: SignUpDto) {
+        console.log("USER", user);
+        return await this.usersRepository.save(user);
+    }
+}
