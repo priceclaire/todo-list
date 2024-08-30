842ad59bad96469de287e48b7476ecc360d0160a
Claire Price <claire.price@mail.com>
2024-08-30 21:40:27 +0100

    add createAccessToken

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {}
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService,
+    ) {}
 
     async hashPassword(password: string) {
         const saltRounds = 10;
         return await bcrypt.hash(password, saltRounds);
     }
 
+    async createAccessToken(user) {
+        const payload = { username: user.username, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
     async signUp(signUpDto) {
         const hashedPassword = await this.hashPassword(signUpDto.password);
-        console.log('HASHED PASSWORD', hashedPassword);
         signUpDto.password = hashedPassword;
-        this.userService.createUser(signUpDto);
-        return "fake token 2";
+
+        const user = await this.userService.createUser(signUpDto);
+        console.log('USER', user);
+
+        return await this.createAccessToken(user);
     }
 }
