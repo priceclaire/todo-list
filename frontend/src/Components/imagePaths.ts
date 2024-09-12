40be084bc22b45da69395c7dff39c85c39caf3fa
Claire Price <claire.price@mail.com>
2024-09-12 23:15:29 +0100

    setup imagePaths

---
+++ frontend/src/Components/imagePaths.ts
@@ -0,0 +1,10 @@
+type ImagePaths = {
+    placeholderLogo: string;
+    [key: string]: string;
+};
+
+const imagePaths: ImagePaths = {
+    placeholderLogo: '/images/placeholder_logo.jpg',
+};
+
+export default imagePaths;
